fd12688b13639d7c635bbff3e7268254f31cf8d6
Abhishek-Jena007 <139780595+Abhishek-Jena007@users.noreply.github.com>
2024-02-12 14:07:59 +0000

    variable first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountNumber = "12345"
+let accountMail = "aj@gmail.com"
+var accountPassword = "98765"
+accountCity = "Odisha"
+
+
+// accountNumber = "56789"
+// now only let is used, var is not in use now a days
+console.log(accountNumber);
+console.table([accountNumber, accountCity, accountMail])
+

bc57cad30313427c33f8acc7563b05c57fb2e433
Abhishek-Jena007 <139780595+Abhishek-Jena007@users.noreply.github.com>
2024-03-05 07:01:56 +0000

    conversion and operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let number = "44abc"
+
+// console.log(typeof number)
+// console.log(typeof (number))
+
+// let changeToNumber = Number(number)
+// console.log(changeToNumber)
+
+//if we will try to covert a string to number like "33" it will be converted easily,but if there is something like "abhi" or "33abc" then it will give the output NaN, which means not a number.
+
+let isLoggedIn = undefined
+let changeToBoolean = Boolean (isLoggedIn)
+console.log(isLoggedIn)
+console.log(changeToBoolean)
+
+//when we convert different data types into boolean 
+//0 => false
+// any number other than 0 gives true
+// any string gives true
+// null, undefined and "" gives false
+

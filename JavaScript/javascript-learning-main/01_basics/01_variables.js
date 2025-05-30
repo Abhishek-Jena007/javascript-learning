5f08880609b944f112e7124a8cbef703e740155a
Abhishek-Jena007 <abhishekjena37@gmail.com>
2025-05-30 10:36:12 +0530

    data types

--- JavaScript/javascript-learning-main/01_basics/01_variables.js
+++ JavaScript/javascript-learning-main/01_basics/01_variables.js
@@ -2,10 +2,10 @@ const accountNumber = "12345"
 let accountMail = "aj@gmail.com"
 var accountPassword = "98765"
 accountCity = "Odisha"
-
+let accountState;
 
 // accountNumber = "56789"
 // now only let is used, var is not in use now a days
 console.log(accountNumber);
-console.table([accountNumber, accountCity, accountMail])
+console.table([accountNumber, accountCity, accountMail, accountState])
 
--- JavaScript/javascript-learning-main/01_basics/02_datatype.js
+++ JavaScript/javascript-learning-main/01_basics/02_datatype.js
@@ -1,2 +1,17 @@
 
 console.log(typeof "abhishek");
+
+console.log(typeof 12345);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof Symbol("symbol"));
+console.log(typeof BigInt(12345678901234567890n));
+console.log(typeof { name: "Abhisek", age: 25 });
+console.log(typeof [1, 2, 3, 4, 5]);
+console.log(typeof function() { return "Hello"; });
+console.log(typeof new Date());
+console.log(typeof /abc/);
+console.log(typeof new Map());
+console.log(typeof new Set());
+
